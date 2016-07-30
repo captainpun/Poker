0b8ae07e927a7badc46ca50f35d38fb43c603fca
Edmon Moren <edmon.moren@gmail.com>
2016-07-29 18:46:39 -0700

    Add files via upload
    - main game is where the game is run
    - table will contain the deck, the community cards, and the players
    
    I added the ability to "deal cards" to the table, and to individual players

--- MainGame.java
+++ MainGame.java
@@ -1,19 +1,22 @@
 package Poker;
+/*
+ * This is the main class for the game, where the main while loop for the rounds will take place etc.
+ *
+ */
+import java.util.ArrayList;
+
 
 public class MainGame {
+	static Table table;
 	
 	public static void main(String[] args){
-		Deck deck = new Deck();
-		deck.shuffleDeck();
-		/*
-		 * uncomment if you want to sort the deck some some reason
-		 * I just created the method to make sure my shuffle wasn't randomly
-		 * messing around with the cards in the deck
-		 * 
-		 */
-		 //deck.sortDeck();
-		 
-		deck.printDeck(true);
+		System.out.println("starting game: ");
+		table = new Table();
+		
+		table.dealCommunityCard();
+		table.dealCommunityCard();
+		table.dealCommunityCard();
+		table.printCommunityCards();
 	}
 }
 
---
+++ Table.java
@@ -0,0 +1,96 @@
+package Poker;
+
+import java.util.ArrayList;
+
+/**
+ * Table is the class that will hold the community cards, and the list of players
+ * 
+ *can deal cards to a player, or deal community cards
+ *
+ * @author edmon.moren
+ *
+ */
+public class Table {
+
+	private Deck deck;
+	private Card[] communityCards;
+	private ArrayList<Player> players;
+
+
+	public Table(){
+		deck = new Deck(true);
+		communityCards = new Card[5];
+		players = new ArrayList<Player>();
+	}
+
+
+	public void getCommunityCard(Card c) throws CommunityCardsFullException{
+		int pos = 0;
+		while(pos < 5){
+			if (communityCards[pos] != null){
+				pos++;
+			} else{
+				//if the position in the array is empty, put the new card there.
+				// exit the function
+				communityCards[pos] = c;
+				return;
+			}
+		}
+		//if the function leaves the while loop, it means that the array holding the community cards is full
+		// throw an exception
+		throw new CommunityCardsFullException(pos, c);
+	}
+	
+	public void dealHoleCard(Player p){
+		try{
+			p.getHoleCard(deck.dealCard() );
+		}catch(HoleCardsFullException e){
+			System.out.println("Player already has " + e.getNumCards() + "cards in the hole");
+			System.out.print("Cannot deal card: ");
+			e.printCardToAdd();
+			e.printStackTrace();
+		}
+	}
+
+	public void dealCommunityCard(){
+		try {
+			this.getCommunityCard(deck.dealCard() );
+		} catch (CommunityCardsFullException e){
+			System.out.println("Table already has " + e.getNumCards() + " community cards");
+			System.out.print("Cannot deal community card: ");
+			e.printCard();
+			e.printStackTrace();
+		}
+	}
+	
+	public void printCommunityCards(){
+		System.out.println("Community Cards: ");
+		for(Card c : communityCards){
+			if(c != null) c.printCardName();
+		}
+	}
+}
+
+
+
+
+
+@SuppressWarnings("serial")
+class CommunityCardsFullException extends Exception{
+	private int numCards;
+	private Card card;
+
+	public CommunityCardsFullException(int numCards, Card c){
+		this.numCards = numCards;
+		this.card = c;
+	}
+
+	public int getNumCards(){
+		return numCards;
+	}
+	public void printCard(){
+		card.printCardName();
+	}
+
+
+}
